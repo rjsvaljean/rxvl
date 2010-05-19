fa05da188963b748e5b004aec29e915894a8d924
Ratan Sebastian <rjsvaljean@gmail.com>
2010-05-19 22:50:35 +0530

    cleaned up the post model

--- models/post.rb
+++ models/post.rb
@@ -1,32 +1,42 @@
+class PostFileMissing
+  
+end
+
 class Post
 
   before :save, :create_slug
   before :save, :set_created_at
 
+  def to_hash
+    {:title => title,
+    :slug => slug,
+    :file_name => file_name,
+    :created_at => created_at}
+  end
+
+  def self.file_exists(file_name)
+    Dir.entries(File.join(APP_ROOT, 'posts')).include?(file_name)
+  end
+
+  def self.add_new_files
+    new_files= self.list_files.delete_if{|i| !Post.all(:file_name => i).empty?}
+    new_files.each do |file|
+      post= Post.new(:file_name => file)
+      post.save
+      post.update_from_file
+      post
+    end
+  end
+
   def self.list_files
-    response= {}
-    Dir.entries(File.join(APP_ROOT, 'posts')).delete_if{|i| i.match(/^\..*$/)}.each do |i|
-      post= Post.all(:file_name => i)
-      if post.empty?
-        response.merge!({i => nil})
-      else
-        response.merge!({i => post[0].id})
-      end
-    end
-    response
+    Dir.entries(File.join(APP_ROOT, 'posts')).delete_if{|i| i.match(/^\..*$/)}
   end
 
-  def self.include?(id)
-    posts= all.select{|i| i.file_name.match(id)}
-    if posts != []
-      true
-    else
-      false
-    end
-  end
-
-  def date
-    created_at.strftime("%d %b") if created_at
+  def update_from_file
+    update(:title => file_title,
+            :created_at => file_created_at,
+            :slug => file_slug,
+            :categories => file_categories)
   end
 
   def full_file_name
@@ -34,39 +44,10 @@ class Post
   end
 
   def hpricot_doc
-    haml= File.read(full_file_name)
-    html= Hpricot(Haml::Engine.new(haml).render)
+    @hpricot_doc ||= Hpricot(Haml::Engine.new(File.read(full_file_name)).render)
   end
 
-  def content
-    (hpricot_doc/'p[@class="content"]').inner_html
-  end
-
-  def snippet
-    (hpricot_doc/'div[@class="snippet"]').inner_html
-  end
-
-  def create_slug
-    self.slug= file_name[3..-6]
-  end
-  
-  def permalink
-    "#{BLOG_BASE}/post/#{file_name[3..-6]}"
-  end
-  
-  def set_created_at
-    begin
-      date= Date.parse((hpricot_doc/'span[@class="date"]').inner_html.strip)
-    rescue
-      date= nil
-    end
-    self.created_at= date || Time.now
-  end
-
-  def self.get_title(file_name)
-    haml= File.read("posts/#{file_name}")
-    html= Haml::Engine.new(haml).render
-    hpricot_doc= Hpricot(html)
+  def file_title
     title= (hpricot_doc/'h1').inner_html
     if title == "" || title == nil
       title= file_name[3..-6].split('-').join(' ').capitalize
@@ -86,39 +67,33 @@ class Post
     file_name[3..-6]
   end
 
-  def self.update(post_id = nil)
-    posts= (Dir.entries('posts').delete_if {|i| i=~ /^\..*$/}).sort
-    if post_id
-      file_name= posts.select{|i| i=~ /#{post_id}-/}
-      if file_name.empty?
-        raise NameError
-      elsif file_name.length > 1
-        puts "Ambiguity between these posts:\n* #{file_name.join("\n* ")}"
-      else
-        file_name= file_name[0]
-        post= Post.all(:file_name => file_name)[0]
-        unless post; post= Post.new(:file_name => file_name); post.save; end
-        post.update(:title => get_title(file_name), 
-                    :created_at => post.file_created_at,
-                    :slug => post.file_slug,
-                    :categories => post.file_categories)
-      end
-    else
-      if Post.last
-        unless posts.last == Post.last.file_name
-          new_post= Post.new(:title => get_title(posts.last),
-                              :file_name => posts.last)
-          new_post.categories= new_post.file_categories
-          new_post.save
-        end
-      else
-        posts.each do |post|
-          new_post= Post.new(:title => get_title(post),
-                              :file_name => post)
-          new_post.categories= new_post.file_categories
-          new_post.save
-        end
-      end
+  def date
+    created_at.strftime("%d %b") if created_at
+  end
+
+
+  def content
+    (hpricot_doc/'p[@class="content"]').inner_html
+  end
+
+  def snippet
+    (hpricot_doc/'div[@class="snippet"]').inner_html
+  end
+
+  def create_slug
+    self.slug= /^(\d*)-(.*)\.(\w*)$/.match(file_name)[2]
+  end
+  
+  def permalink
+    "#{BLOG_BASE}/post/#{file_name[3..-6]}"
+  end
+  
+  def set_created_at
+    begin
+      date= Date.parse((hpricot_doc/'span[@class="date"]').inner_html.strip)
+    rescue
+      date= nil
     end
+    self.created_at= date || Time.now
   end
 end
