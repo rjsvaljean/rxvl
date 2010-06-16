445c697065ad6eb84caf0cbcab700db6c64d27b7
root <root@localhost.localdomain>
2010-06-16 19:38:41 +0530

    wiped out rxvl1

--- models/base.rb
+++
@@ -1,8 +0,0 @@
-require 'dm-core'
-require 'haml'
-require 'hpricot'
-
-load File.join(APP_ROOT,'db','migrations.rb')
-(Dir.entries(File.join(APP_ROOT,'models')).delete_if {|i| i =~ /^\..*/ || i == "base.rb"}).each do |file|
-  load File.join APP_ROOT, 'models', file
-end
